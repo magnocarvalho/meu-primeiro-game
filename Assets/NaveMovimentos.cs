cc4cacb7b7f0df976de09c6741e936fba72d4782
Magno Carvalho <magnocarv@hotmail.com>
2019-03-14 02:27:01 -0300

    aula de jogos segunda

---
+++ Assets/NaveMovimentos.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public float velocidade = 0.5F;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector2(x, 0));
+    }
+}
