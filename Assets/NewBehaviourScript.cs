4ca9d7cde5869f0270aa2070ebec9d1de3d51b89
Magno Carvalho <magnocarv@hotmail.com>
2019-03-18 19:58:12 -0300

    primeiro script

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-
-    public float velocidade = 0.5F;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
         float x = Input.GetAxis("Horizontal");
-        transform.Translate(new Vector2(x, 0));
+
+        this.transform.Translate(new Vector2(x, 0));
+
     }
 }
