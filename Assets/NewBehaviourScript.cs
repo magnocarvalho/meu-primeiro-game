cd41c908d832f8879f43d9b9dc1b1ba73eaa230f
Magno Carvalho <magnocarv@hotmail.com>
2019-03-18 20:52:11 -0300

    fazer o bagulho funcionar no horizontal

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     public float velocidade;
+    private float largura;
     // Start is called before the first frame update
     void Start()
     {
-       
+        largura = Camera.main.orthographicSize * Camera.main.aspect; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
+        float dirX = Input.GetAxis("Horizontal");
+        float dirY = Input.GetAxis("Vertical");
 
-        transform.Translate(new Vector2(x, 0) *velocidade);
+        transform.Translate(new Vector2(dirX, dirY) * Time.deltaTime * velocidade);
 
+        if(transform.position.x > largura)
+        {
+            transform.position = new Vector2(-largura, transform.position.y);
+        }
+        else if(transform.position.x < -largura)
+        {
+            transform.position = new Vector2(largura, transform.position.y);
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# meu-primeiro-game
-IF68X - Desenvolvimento de Jogos - C91 (2019_01)
+# Primeiro Projeto de Game
+## IF68X - Desenvolvimento de Jogos - C91 (2019_01)
+
+Jogo ARCADE desenvolvido na sala de aula, durante a disciplina de Desenvolviemnto de Jogos
