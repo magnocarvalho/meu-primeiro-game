9bfb5a8c34aa71de261567b92c029978c11ade6b
Magno Carvalho <magnocarv@hotmail.com>
2019-03-25 21:00:38 -0300

    ta funcionando o joguinho =)

--- Assets/navemal.cs
+++ Assets/navemal.cs
@@ -6,10 +6,13 @@ public class navemal : MonoBehaviour
 {
     private Rigidbody2D mal;
     public float velocidade;
+    public float altura;
     // Start is called before the first frame update
     void Start()
     {
+        altura = Camera.main.orthographicSize;
         velocidade = 6;
+
         mal = this.GetComponent<Rigidbody2D>();
         mal.velocity = new Vector2(0, -1*velocidade);
     }
@@ -17,6 +20,9 @@ public class navemal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.y < -altura - 1)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
