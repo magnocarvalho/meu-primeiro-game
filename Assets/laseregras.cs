6b9731174ce1e84127bc03562f60e3eb7632ed1e
Magno Carvalho <magnocarv@hotmail.com>
2019-03-27 20:26:05 -0300

    tiro caindo de cima

---
+++ Assets/laseregras.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laseregras : MonoBehaviour
+{
+    private Rigidbody2D mal;
+    public float velocidade;
+    public float altura;
+    // Start is called before the first frame update
+    void Start()
+    {
+        altura = Camera.main.orthographicSize;
+        velocidade = 6;
+
+        mal = this.GetComponent<Rigidbody2D>();
+        mal.velocity = new Vector2(0, -1 * velocidade);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -altura - 1)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/laserosa.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laserosa : MonoBehaviour
+{
+    private float largura;
+    private float altura;
+    float raa;
+    public GameObject laser;
+    void Start()
+    {
+
+        altura = Camera.main.orthographicSize;
+        largura = altura * Camera.main.aspect;
+
+        InvokeRepeating("respaw", 3, 0.5f);
+        respaw();
+
+    }
+    private void respaw()
+    {
+        raa = Random.Range(-largura, largura);
+        Vector2 posicao = new Vector2(raa, altura + 1);
+        Instantiate(laser, posicao, Quaternion.identity);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
